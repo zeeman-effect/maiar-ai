97f00e1d897dc79ed96f9bdbec87110ffa32867c
ktn1234 <ktn1234@yahoo.com>
2025-03-27 20:37:00 -0500

    refactor(core): 🔥 remove unused system template

--- packages/core/src/system/templates.ts
+++
@@ -1,7 +0,0 @@
-export function generateSystemTemplate(): string {
-  return `
-You have rules that you must follow:
-1. Always be precise when performing internal operations.
-2. Do not hallucinate or make up commands, requests, actions, operations, etc.
-    `;
-}
