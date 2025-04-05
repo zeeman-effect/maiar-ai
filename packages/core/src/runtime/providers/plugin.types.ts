e78b86571fe46eb5f0167866727fbc083991a54d
ktn1234 <ktn1234@yahoo.com>
2025-04-05 16:25:07 -0500

    chore(core): 🔥 remove plugin types dead code comments

--- packages/core/src/runtime/providers/plugin.types.ts
+++ packages/core/src/runtime/providers/plugin.types.ts
@@ -1,17 +1,5 @@
 import { AgentContext } from "../pipeline/agent";
 
-// type Success<T> = {
-//   data: T;
-//   error: null;
-// };
-
-// type Failure<E> = {
-//   data: null;
-//   error: E;
-// };
-
-// type Result<T, E = Error> = Success<T> | Failure<E>;
-
 /**
  * Result of a plugin execution
  */
@@ -43,16 +31,6 @@ export interface Executor {
    * @returns {Promise<PluginResult>} A promise resolving to the result of execution.
    */
   fn: (context: AgentContext) => Promise<PluginResult> | PluginResult;
-
-  /**
-   * Executes the plugin logic with the given agent context.
-   *
-   * @template T The expected successful result type.
-   * @template E The error type, defaults to `Error`.
-   * @param {AgentContext} context - The execution context for the agent.
-   * @returns {Promise<Result<T, E>>} A promise resolving to the result of execution.
-   */
-  // execute<T, E = Error>(context: AgentContext): Promise<Result<T, E>>;
 }
 
 /**
