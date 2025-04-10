93850104556df49a92530239fddf582eda4e1bf0
0xPBIT <contact@pbit.us>
2025-04-10 16:59:48 -0500

    chore: add introspection endpoint

--- packages/core/src/runtime/managers/server.ts
+++ packages/core/src/runtime/managers/server.ts
@@ -38,10 +38,32 @@ export class ServerManager {
     );
   }
 
+  private mountIntrospectionRoute(): void {
+    this.app.get("/introspect", (_req: Request, res: Response) => {
+      res.json({
+        routes: this.app._router.stack
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          .filter((r: any) => r.route)
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          .map((r: any) => ({
+            path: r.route.path,
+            methods: Object.keys(r.route.methods).reduce(
+              (acc: Record<string, boolean>, method: string) => {
+                acc[method] = r.route.methods[method];
+                return acc;
+              },
+              {}
+            )
+          }))
+      });
+    });
+  }
+
   public async start(): Promise<void> {
     this.app.use(express.json());
-
     this.app.use(cors(this.cors));
+
+    this.mountIntrospectionRoute();
 
     this._server = this.app.listen(this.port);
   }
