88cace0e7e49524168d14fff9b6a9b5037a1858c
0xPBIT <contact@pbit.us>
2025-04-10 17:02:13 -0500

    chore: remove logger ops

--- packages/core/src/lib/logger/index.ts
+++ packages/core/src/lib/logger/index.ts
@@ -2,7 +2,7 @@ import { createLogger, transports } from "winston";
 
 import { stdout, websocket } from "../winston";
 
-export { Logger, LoggerOptions, transports } from "winston";
+export { Logger, transports } from "winston";
 export { stdout, websocket };
 
 export default createLogger({
